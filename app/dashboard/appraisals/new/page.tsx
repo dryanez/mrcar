ff74767b19c2bb0f0b3be9e385166e64d387f209
jpblest <jpblest@gmail.com>
2026-02-10 12:40:23 -0300

    fix: change traccion select to use string array - fixes React error #31

--- app/dashboard/appraisals/new/page.tsx
+++ app/dashboard/appraisals/new/page.tsx
@@ -503,12 +503,7 @@ export default function NewAppraisalPage() {
                                 <FormSelect
                                     label="Tracción"
                                     {...register('traccion')}
-                                    options={[
-                                        { value: '', label: 'Seleccionar...' },
-                                        { value: '4x2', label: '4x2' },
-                                        { value: '4x4', label: '4x4' },
-                                        { value: 'AWD', label: 'AWD' },
-                                    ]}
+                                    options={['4x2', '4x4', 'AWD']}
                                 />
 
                                 {/* NEW: Línea de Asientos */}
